3ddfb0a173544c6da7e5ea0a3186b8d7b044d5fb
Christofer Bertonha <christofer.bertonha@typeform.com>
2020-12-07 13:39:47 +0100

    Add fade

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 import pygame
 
 
-running = True
-
-pygame.init()
-pygame.mouse.set_visible(False)
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen.fill(pygame.Color(0, 0, 0))
-imgsize = pygame.Surface.get_size(screen)
-
-images = ["1.jpg", "2.jpg", "3.jpg"]
-image_index = 1
-
-
 def load_image(index):
-    image = f'{index}.jpg'
-    background = pygame.image.load(image)
-    background = pygame.transform.scale(background, (imgsize[0], imgsize[1]))
-    screen.blit(background, (0, 0))
-    pygame.display.update()
+    background = pygame.image.load(f'{index}.jpg')
+    background = pygame.transform.scale(background, (img_size[0], img_size[1]))
+    background = background.convert_alpha()
+    return background
 
 
 def inc_index(index):
@@ -28,13 +15,37 @@ def inc_index(index):
     return new_index
 
 
-while running:
-    load_image(image_index)
+def fade(background):
+    for alpha in range(0, 300, 2):
+        screen.fill(pygame.Color(0, 0, 0))
+        background.set_alpha(alpha)
+        screen.blit(background, (0, 0))
+        pygame.display.update()
+        pygame.time.delay(5)
 
+
+running = True
+
+pygame.init()
+pygame.mouse.set_visible(False)
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen.fill(pygame.Color(0, 0, 0))
+img_size = pygame.Surface.get_size(screen)
+
+image_index = 1
+
+# load initial image
+background = load_image(image_index)
+screen.blit(background, (0, 0))
+pygame.display.update()
+
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             image_index = inc_index(image_index)
-            load_image(image_index)
+            background = load_image(image_index)
+            fade(background)
 
         if event.type == pygame.KEYDOWN:
             running = False
